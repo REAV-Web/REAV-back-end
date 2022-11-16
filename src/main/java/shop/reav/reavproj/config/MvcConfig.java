a5cac3648dd810391b4f6bf9d5300fc49e9682fe
민재 권 <mjkweon17@korea.ac.kr>
2022-11-16 22:56:45 +0900

    test - enable cors

--- src/main/java/shop/reav/reavproj/config/MvcConfig.java
+++ src/main/java/shop/reav/reavproj/config/MvcConfig.java
@@ -11,7 +11,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods(HttpMethod.GET.name());
+                .allowedOrigins("*");
     }
 }
